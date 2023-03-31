59bce62295f896208bc630cdd4cebfed53992656
Sylvia Mumbi <mumbi099@gmail.com>
2023-03-31 17:42:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+//task1
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+//task2
+let random = Math.floor(Math.random() * 100);
+console.log(random);
+
+//task3
+let num3 = 24;
+let num4 = 5;
+
+let mod = num3 % num4;
+console.log(mod);
+
+//task4
+let numbers = [3, 9, 20, 19, 12, 15, 6]; 
+let max = Math.max(...numbers); 
+
+console.log(max);
